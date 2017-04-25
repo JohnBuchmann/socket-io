fbb947119bbb6cdb0edb2bbfc8256955af5a85bd
John Buchmann <jsbuchmann@gmail.com>
2017-04-25 16:11:24 -0400

    Updated package.json

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-// aaa
-// ddd
-// last comment
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.send('App works!!!');
+})
+
+app.set('view engine', 'ejs');
+
+app.listen(port);
