ad4938c118037bda9ec16b181bc0d5ea513f00cf
John Buchmann <jsbuchmann@gmail.com>
2017-04-27 11:31:33 -0400

    Added timestamps using Moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,20 @@ socket.on('disconnect', function() {
 })
 
 socket.on('newMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('newMessage', message);
     var li = $('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
     var a = $('<a target="_blank">My current location</a>')
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     $('#messages').append(li);
@@ -31,11 +33,13 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageTextbox = $('#inputmessage');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('#inputmessage').val()
+        text: messageTextbox.val()
     }, function() {
-
+        messageTextbox.val('');
     })
 });
 
@@ -45,12 +49,16 @@ locationButton.on('click', function(e) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function() {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('unable to fetch location');
     })
 })
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,3 +1,6 @@
+
+
+
 var generateMessage = (from, text) => {
     return {
         from: from,
